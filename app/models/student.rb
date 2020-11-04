fdaf09989dee6c5af892683593571bbcd5997866
Travis Lee <travisslee223@gmail.com>
2020-11-04 23:51:51 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,11 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+   def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path
+  end
 end
