e8342595a244906a83cc36ceee9029a7872d61bf
Travis Lee <travisslee223@gmail.com>
2020-11-04 23:38:18 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,12 @@
 class Student < ActiveRecord::Base
   
-  
+  after_initialize :active
   
   def to_s
     self.first_name + " " + self.last_name
+  end
+  
+  def active
+    self.active = false
   end
 end
